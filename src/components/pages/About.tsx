b1653f709bfc2107107f2d7d7a6db7590afb9b42
Zuby <38223403+ZubyWasTaken@users.noreply.github.com>
2025-08-19 04:50:13 +0100

    remove string not needed in about page

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -6,7 +6,6 @@ function About() {
       <Row>
         <Col>
           <h1 className="display-4 text-center mb-4">About Mailboxer</h1>
-          {/* <p className="lead text-center">Learn more about Mailboxer and our research.</p> */}
         </Col>
       </Row>
     </Container>
